27e6b7ed7c8ff53567b2a15c7c98faafa8d6150c
James Hobson <james.hobson@oriel.ox.ac.uk>
2019-03-15 16:13:05 +0000

    Added State Blitting

--- Server/ProjectM.hs
+++ Server/ProjectM.hs
@@ -35,19 +35,20 @@ findInput xs s = (safeHead $ filter (\(a,_) -> a == s) xs) >>= return . snd
 findInputM :: (Monad m) => [Input] -> String -> m (Maybe String)
 findInputM ins = return . findInput ins
 
-blitResult :: (Monad m) => Maybe (a, Result) -> m Result
-blitResult (Just (_,r)) = return r
-blitResult Nothing      = return Yeet
+blitResult :: (Show a) => DBName -> Maybe a -> Maybe (a, Result) -> IO Result
+blitResult db _ (Just (s,r))   = connect defaultConnectInfo >>= (\conn -> runRedis conn (lpush (BC.pack db) [BC.pack $ show s])) >> return r
+blitResult db (Just i) Nothing = connect defaultConnectInfo >>= (\conn -> runRedis conn (lpush (BC.pack db) [BC.pack $ show i])) >> return Yeet --reblit initial state i
+blitResult _  Nothing Nothing  = return Yeet --never got initial state, do nothing
 
-cgiMain :: (Read b) => Updater a b -> Maybe a -> CGI CGIResult
-cgiMain u init = getInputs >>= flip findInputM "event" |>>=| readMaybe |>>=| (\e -> (init >>= \i -> return (u i e))) >>= blitResult >>= return . show >>= output
+cgiMain :: (Read b, Show a) => DBName -> Updater a b -> Maybe a -> CGI CGIResult
+cgiMain db u init = getInputs >>= flip findInputM "event" |>>=| readMaybe |>>=| (\e -> (init >>= \i -> return (u i e))) >>= \s -> liftIO (blitResult db init s) >>= return . show >>= output
 
 existsDB :: String -> IO (Bool)
 existsDB db = connect defaultConnectInfo >>= (\conn -> runRedis conn (exists $ BC.pack db)) >>= normalize
     where normalize (Left _)  = return False
           normalize (Right i) = return i
 
-getState :: (Read a) => String -> a -> IO (Maybe a)
+getState :: (Read a, Show a) => String -> a -> IO (Maybe a)
 getState db _ = connect defaultConnectInfo >>= (\conn -> runRedis conn (blpop [BC.pack db] 0)) >>= normalize |>>=| readMaybe . BC.unpack . snd
     where normalize (Left _)  = return Nothing
           normalize (Right i) = return i
@@ -59,5 +60,5 @@ branch :: (a -> b) -> (a -> b) -> Bool -> (a -> b)
 branch f _ True  = f
 branch _ f False = f
 
-runSite :: (Read a, Read b, Show a) => DBName -> Updater a b -> a -> IO ()
-runSite db update init = existsDB db >>= \p -> branch (getState db) (setState db) p init >>= \i ->  runCGI (handleErrors $ cgiMain update i)
+runSite :: (Read a, Read b, Show a, Show b) => DBName -> Updater a b -> a -> IO ()
+runSite db update init = existsDB db >>= \p -> branch (getState db) (setState db) p init >>= \i ->  runCGI (handleErrors $ cgiMain db update i)
