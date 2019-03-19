43d0a01ce0f8122e490a68c068e1ba5ac731530f
James Hobson <james.hobson@oriel.ox.ac.uk>
2019-03-19 21:00:39 +0000

    Added loggin class

--- App/ProjectM/testpage.cpp
+++ App/ProjectM/testpage.cpp
@@ -1,14 +1,59 @@
 #include "testpage.h"
 #include "ui_testpage.h"
+#include <QDebug>
 
 TestPage::TestPage(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::TestPage)
 {
     ui->setupUi(this);
+    logger = Logger(ui->logPage);
 }
 
 TestPage::~TestPage()
 {
     delete ui;
+}
+
+Logger::Logger(QPlainTextEdit *edit) : target(edit)
+{
+    if (target == nullptr) return;
+    *this << "Logger initialised";
+}
+
+Logger::Logger(const QPlainTextEdit *edit, int ind) : target(edit)
+{
+    indent = ind;
+}
+
+Logger Logger::operator<<(QString string)
+{
+    printIndent(indent);
+    QString tmp = target->toPlainText();
+    tmp = tmp + string + '\n';
+    target->document()->setPlainText(tmp);
+    return *this;
+}
+
+Logger Logger::operator<<(const char *string)
+{
+    QString str = QString::fromLatin1(string);
+    return operator<<(str);
+}
+
+Logger Logger::operator[](int ind)
+{
+    return Logger(target, ind);
+}
+
+void Logger::printIndent(int i)
+{
+    QString tmp = target->toPlainText();
+    for(int j = 0; j < i; j++) tmp += "    ";
+    target->document()->setPlainText(tmp);
+}
+
+void TestPage::on_get_projects_btn_clicked()
+{
+    logger << "Getting Projects";
 }
--- App/ProjectM/testpage.h
+++ App/ProjectM/testpage.h
@@ -2,10 +2,32 @@
 #define TESTPAGE_H
 
 #include <QMainWindow>
+#include <QPlainTextEdit>
 
 namespace Ui {
 class TestPage;
 }
+
+// Logger class for controlling the logger
+// 'logger << "Hello" << "World"' will produce:
+// Hello
+// World
+// If you want an indent, logger[n] will indent n times for everything on that line!
+
+class Logger {
+public:
+    Logger(QPlainTextEdit *edit = nullptr);
+
+    Logger operator<<(QString string);
+    Logger operator<<(const char *string);
+    Logger operator[](int ind);
+
+private:
+    Logger(const QPlainTextEdit *edit, int ind);
+    const QPlainTextEdit *target;
+    void printIndent(int i);
+    int indent = 0;
+};
 
 class TestPage : public QMainWindow
 {
@@ -15,8 +37,12 @@ public:
     explicit TestPage(QWidget *parent = nullptr);
     ~TestPage();
 
+private slots:
+    void on_get_projects_btn_clicked();
+
 private:
     Ui::TestPage *ui;
+    Logger logger;
 };
 
 #endif // TESTPAGE_H
