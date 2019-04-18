1df3865105cc07125d5552e2805e51214aa19eaf
James Hobson <james.hobson@oriel.ox.ac.uk>
2019-04-18 14:03:00 +0100

    Added URL to settings

--- App/ProjectM/projectwindow.cpp
+++ App/ProjectM/projectwindow.cpp
@@ -31,6 +31,12 @@ ProjectWindow::ProjectWindow(Project *project, QWidget *parent) :
 
     assert(settings.contains(ProjectSettings::CHARGING_ONLY));
     bool pluggedin_setting = settings.value(ProjectSettings::CHARGING_ONLY).toBool();    ui->plugged_in_check_box->setCheckState(pluggedin_setting ? Qt::Checked : Qt::Unchecked);
+
+    // Ensures that if the project is enabled, URL already set
+    QString q_url_setting_key = project->name() + "_url_setting";
+    settings.setValue(q_url_setting_key,project->url());
+
+
 }
 
 ProjectWindow::~ProjectWindow()
