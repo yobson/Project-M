07d3531d28e67eb3cdebd0c291a4bcb728fb20cc
George Chichirim <georgechichirim@gmail.com>
2019-04-18 23:33:32 +0300

    Clear the service when the service is started

--- App/ProjectM/projectwindow.cpp
+++ App/ProjectM/projectwindow.cpp
@@ -17,7 +17,6 @@ ProjectWindow::ProjectWindow(Project *project, QWidget *parent) :
                        | Qt::WindowMaximized);
     ui->project_name->setText(project->name());
     ui->project_desc->setText(project->full_desc());
-
     //Set checkboxes to the saved setting, or unchecked if this project has never been viewed before
     QSettings settings(COMPANY_NAME, APP_NAME);
     settings.beginGroup(ALL_PROJECTS_DIR);
@@ -32,8 +31,13 @@ ProjectWindow::ProjectWindow(Project *project, QWidget *parent) :
     ui->wifi_check_box->setCheckState(wifi_setting ? Qt::Checked : Qt::Unchecked);
 
     assert(settings.contains(ProjectSettings::CHARGING_ONLY));
-    bool pluggedin_setting = settings.value(ProjectSettings::CHARGING_ONLY).toBool();
-    ui->plugged_in_check_box->setCheckState(pluggedin_setting ? Qt::Checked : Qt::Unchecked);
+    bool pluggedin_setting = settings.value(ProjectSettings::CHARGING_ONLY).toBool();    ui->plugged_in_check_box->setCheckState(pluggedin_setting ? Qt::Checked : Qt::Unchecked);
+
+    // Ensures that if the project is enabled, URL already set
+    QString q_url_setting_key = project->name() + "_url_setting";
+    settings.setValue(q_url_setting_key,project->url());
+
+
 }
 
 ProjectWindow::~ProjectWindow()
@@ -45,7 +49,6 @@ ProjectWindow::~ProjectWindow()
 void ProjectWindow::on_enabled_check_box_stateChanged(int arg1)
 {
     this->project->enabled() = arg1;
-
     QSettings settings(COMPANY_NAME, APP_NAME);
     settings.beginGroup(ALL_PROJECTS_DIR);
     settings.beginGroup(this->project->name());
@@ -75,6 +78,7 @@ void ProjectWindow::on_plugged_in_check_box_stateChanged(int arg1)
     QSettings settings(COMPANY_NAME, APP_NAME);
     settings.beginGroup(ALL_PROJECTS_DIR);
     settings.beginGroup(this->project->name());
+
 
     settings.setValue(ProjectSettings::CHARGING_ONLY, this->project->plugged_in_only());
     settings.sync();
