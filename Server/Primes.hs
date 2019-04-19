4a59905f60e4f193548dd1fe42690d191a9a5984
James Hobson <james.hobson@oriel.ox.ac.uk>
2019-04-19 18:10:58 +0100

    Added autorefresh

--- Server/Primes.hs
+++ Server/Primes.hs
@@ -5,7 +5,7 @@ type State = (Int, Int, Int) -- Largest, Last Checked, ID of largest
 jsMin = "(function(id, number){var n = parseInt(number); for (var i = 2; i <= Math.sqrt(n); i++) {if (n % i == 0){return id + \" \" + 0}} return id + \" \" + n;})" -- JS goes here
 
 genPage :: Int -> Int -> String
-genPage prime id = concat ["<head><script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>",
+genPage prime id = concat ["<head><meta http-equiv=\"refresh\" content=\"5\"><script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>",
   "<script>var name = \"\"; jQuery.getJSON(\"/cgi-bin/main.cgi?action=GetUser&id=", show id, "\",{format: \"json\"}).done(function (json) {name = json.firstName + \" \" + json.lastName; $(\"#finder\").html(\"Finder: \" + name)});</script>",
   "</head><body><h2>Largest prime found is ", show prime, "</h2>",
   "<h3 id=\"finder\">Finder: ", show id,
